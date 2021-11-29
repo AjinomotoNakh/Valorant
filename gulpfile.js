45df84e578437dca38f5d6b954b8f893300d371f
AjinomotoNakh <mob333@mail.ru>
2021-11-29 11:58:08 +1000

    inerit commit

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@ let path = {
 	},
 	watch: {
 		html: source_folder + "/**/*.html",
-		css: source_folder + "/scss/**/*.scss",
+		css: source_folder + "/scss/*.scss",
 		js: source_folder + "/js/**/*.js",
 		img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
 	},
@@ -72,9 +72,7 @@ function html() {
 
 function css() {
 	return src(path.src.css)
-		.pipe(scss({
-			outputStyle: "expanded"
-		}))
+		.pipe(scss({ outputStyle: 'expanded' }).on('error', scss.logError))
 		.pipe(group_media())
 		.pipe(autoprefixer({
 			overrideBrowserslist: ['last 5 versions'],
@@ -158,9 +156,9 @@ function cb() {
 }
 
 function watchFiles(params) {
-	gulp.watch([path.watch.html], html),
-		gulp.watch([path.watch.css], css),
-		gulp.watch([path.watch.js], js);
+	gulp.watch([path.watch.html], html);
+	gulp.watch([path.watch.css], css);
+	gulp.watch([path.watch.js], js);
 	gulp.watch([path.watch.img], images);
 }
 
@@ -190,8 +188,8 @@ function clean(params) {
 	return del(path.clean);
 }
 
-let build = gulp.series(clean, gulp.parallel(js, css, html, images, fonts), fontsStyle);
-let watch = gulp.parallel(build, watchFiles, browserSync);
+let build = gulp.series(clean, gulp.parallel(js, css, html, images, fonts), gulp.parallel(fontsStyle, browserSync));
+let watch = gulp.parallel(build, watchFiles);
 
 
 exports.fontsStyle = fontsStyle;
